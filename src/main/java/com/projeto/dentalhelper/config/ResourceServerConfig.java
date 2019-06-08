97d42f35ca2fe247a4ed3e77c1ac4e356b40dc89
Leocaliban <leocaliban@gmail.com>
2019-06-08 03:10:19 -0300

    FIX Permite acesso à documentação da api com swagger sem autenticação

--- src/main/java/com/projeto/dentalhelper/config/ResourceServerConfig.java
+++ src/main/java/com/projeto/dentalhelper/config/ResourceServerConfig.java
@@ -14,18 +14,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.anyRequest().authenticated()
-				.and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.csrf().disable();
+		http.authorizeRequests().antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+				.permitAll().anyRequest().authenticated().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
 	}
-	
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(true);
 	}
-	
+
 	@Bean
 	public MethodSecurityExpressionHandler createExpressionHandler() {
 		return new OAuth2MethodSecurityExpressionHandler();
